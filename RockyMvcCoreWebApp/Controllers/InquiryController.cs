ed8471288c78853494e4d49ea8a4be857b78f8e1
mustafaozdemir27 <42694496+mustafaozdemir27@users.noreply.github.com>
2021-09-25 21:07:03 +0300

    InquiryCompleted

--- RockyMvcCoreWebApp/Controllers/InquiryController.cs
+++ RockyMvcCoreWebApp/Controllers/InquiryController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RockyMvcCoreWebApp_DataAccess.Repository.IRepository;
+using RockyMvcCoreWebApp_Models.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,8 @@ namespace RockyMvcCoreWebApp.Controllers
         private readonly IInquiryHeaderRepository _inqHRepo;
         private readonly IInquiryDetailRepository _inqDRepo;
 
+        [BindProperty] // For post all the data
+        public InquiryVM InquiryVM { get; set; }
 
         public InquiryController(IInquiryHeaderRepository inqHRepo, IInquiryDetailRepository inqDRepo)
         {
@@ -23,5 +26,24 @@ namespace RockyMvcCoreWebApp.Controllers
         {
             return View();
         }
+
+        public IActionResult Details(int id)
+        {
+            InquiryVM = new InquiryVM()
+            {
+                InquiryHeader = _inqHRepo.FirstOrDefault(u => u.Id == id),
+                InquiryDetail = _inqDRepo.GetAll(u => u.InquiryHeaderId == id, includeProperties: "Product")
+            };
+
+            return View(InquiryVM);
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetInquiryList()
+        {
+            return Json(new { data = _inqHRepo.GetAll() });
+        }
+        #endregion
     }
 }
